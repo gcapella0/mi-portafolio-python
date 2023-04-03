133078578a8a03e67d42a9cd4e619acd2e0bad3b
Genghis Capella <gcapella1991@gmail.com>
2023-04-02 23:15:49 -0300

    Add files via upload

---
+++ generarcontraseñas.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+
+def generate_password(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+    return result_str
+
+
+print (generate_password(8))
