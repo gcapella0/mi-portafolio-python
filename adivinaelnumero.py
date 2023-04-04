431fb223c65731bbe212ce2c802a81eb139a0bdf
Genghis <gcapella1991@gmail.com>
2023-04-03 23:21:08 -0300

    Refactorizando

--- adivinaelnumero.py
+++ adivinaelnumero.py
@@ -2,11 +2,11 @@ import random
 n = random.randint(1, 10) 
 adivino = int(input("Adivina el número entre 1 y 10: ")) 
 while n != "adivino": 
-    print 
-    if adivino < n: 
-        print ("Demasiado bajo") 
-    elif adivino > n: 
-        print ("Demasiado alto") 
-    adivino = int(input("Adivina el número entre 1 y 10: ")) 
+	print 
+	if adivino < n: 
+		print ("Demasiado bajo") 
+	elif adivino > n: 
+		print ("Demasiado alto") 
+	adivino = int(input("Adivina el número entre 1 y 10: ")) 
 if adivino == n: 
-    print ("¡Correcto!")
+	print ("¡Correcto!")
